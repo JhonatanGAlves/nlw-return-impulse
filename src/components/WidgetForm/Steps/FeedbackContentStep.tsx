5130b83df44f84f869de7b3e4f129eadc16e31ba
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-05-04 08:24:24 -0300

    FeedbackContentStep component created and ScreenshotButton component imported

---
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,81 @@
+import { FormEvent, useState } from 'react'
+import { ArrowLeft } from 'phosphor-react'
+import { FeedbackTypes, feedbackTypes } from '..'
+import { CloseButton } from '../../CloseButton'
+import { ScreenshotButton } from '../ScreenshotButton'
+
+// To dizendo que o tipo do valor que foi passado em feedbackType lá no component index é do tipo FeedbackTypes
+// ("BUG", "IDEA" e "OTHER")
+type FeedbackContentStepProps = {
+  feedbackType: FeedbackTypes
+  onFeedbackRestartRequested: () => void
+  onFeedbackSent: () => void
+}
+
+export const FeedbackContentStep = ({
+  feedbackType,
+  onFeedbackRestartRequested,
+  onFeedbackSent
+}: FeedbackContentStepProps) => {
+  const [screenshot, setScreenshot] = useState<string | null>(null)
+  const [comment, setComment] = useState('')
+  // Importei feedbackTypes que é um objeto declarado lá no index e e passei em forma de array somente as keys pra ele
+  // ("BUG", "IDEA" e "OTHER"). Com isso, a variável feedbackTypeInfo passou a ter acesso as propriedades de cada
+  // chave.
+  const feedbackTypeInfo = feedbackTypes[feedbackType]
+
+  const handleSubmitFeedback = (event: FormEvent) => {
+    event.preventDefault()
+
+    console.log({
+      comment,
+      screenshot
+    })
+
+    onFeedbackSent()
+  }
+
+  return (
+    <>
+      <header>
+        <button
+          type="button"
+          className="absolute top-5 left-5 text-zinc-400 hover:text-zinc-100"
+          onClick={onFeedbackRestartRequested}
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4" />
+        </button>
+        <span className="text-xl leading-6 flex items-center gap-2">
+          <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
+          {feedbackTypeInfo.title}
+        </span>
+        <CloseButton />
+      </header>
+
+      <form
+        onSubmit={handleSubmitFeedback}
+        className="my-4 w-full">
+        <textarea
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-1 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+          onChange={event => setComment(event.target.value)}
+          placeholder="Conte com detalhes o que está acontecendo..."
+        />
+
+        <footer className="flex gap-2 mt-2">
+          <ScreenshotButton
+            screenshot={screenshot}
+            onScreenshotTook={setScreenshot}
+          />
+          <button
+            type="submit"
+            // Ou seja, se não tiver nada escrito no textarea, desabilita esse botão.
+            disabled={comment.length === 0}
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+          >
+            Enviar feedback
+          </button>
+        </footer>
+      </form>
+    </>
+  )
+}
